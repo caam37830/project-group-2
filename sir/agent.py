610556528582ecd7613479b7664fddaf08cf2c30
jiangmengli19 <jiangmengl@uchicago.edu>
2020-11-06 21:00:07 -0600

    submit necessary file

---
+++ sir/agent.py
@@ -0,0 +1,74 @@
+import numpy as np
+import matplotlib.pyplot as plt
+class Person():
+    """
+    An agent representing a person.
+
+    """
+
+    def __init__(self):
+        self.infected = False # if enlightened = True, the person has heard idea
+        self.willinfect = True
+
+    def is_infected(self):
+        """
+        returns true if the person has  been infected
+        """
+        return self.infected
+
+    def is_willinfected(self):
+        """
+        return true if the person is Susceptible
+        """
+        return self.willinfect
+
+
+    def get_infected(self):
+        """
+        once the person get infected
+        """
+        self.infected = True
+        self.willinfect = False
+
+    def get_recovered(self):
+        """
+        once the person get recovered
+        """
+        self.infected = False
+
+    def is_recovered(self):
+        """
+        if the person is recovered
+        """
+        return self.infected==False and self.willinfect == False
+
+
+def count_infect(pop):
+    """
+    the total infected people
+    """
+    total = 0
+    for i in pop:
+        if i.is_infected():
+            total = total + 1
+    return total
+
+def count_recover(pop):
+    """
+    the total recovered people
+    """
+    total = 0
+    for i in pop:
+        if i.is_recovered():
+            total = total + 1
+    return total
+
+def count_suspectial(pop):
+    """
+    the total suspectial people
+    """
+    total = 0
+    for i in pop:
+        if i.is_willinfected():
+            total = total + 1
+    return total
