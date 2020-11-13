7c54d2b08e8c6a642d89bfaaa9911dc3be248b2e
jiangmengli19 <jiangmengl@uchicago.edu>
2020-11-13 09:55:56 -0600

    we do the modification

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ class Testcountinfect(unittest.TestCase):
       """
       This test is used for testing the total people infected
       """
-      def test_three(self):
+      def test_suspectial(self):
             N = 2000
             susceptible  = []
             infect = []
@@ -26,7 +26,31 @@ class Testcountinfect(unittest.TestCase):
                         recover.append(pop[i])
                         pop[i].get_infected()
                         pop[i].get_recovered()
-
             self.assertTrue(count_suspectial(pop)==len(susceptible))
+
+      def test_infectremove(self):
+            """
+            We test the percentage of infected and removed people
+            """
+            N = 2000
+            susceptible  = []
+            infect = []
+            recover = []
+            pop = [Person() for i in range(N)]
+
+            for i in range(N):
+                  k = np.random.randint(3,size = 1)
+                  if k == 0:
+                        susceptible.append(pop[i])
+
+                  if k == 1:
+                        infect.append(pop[i])
+                        pop[i].get_infected()
+                  if k == 2:
+                        recover.append(pop[i])
+                        pop[i].get_infected()
+                        pop[i].get_recovered()
+            #We test the amount of infected people
+
             self.assertTrue(count_infect(pop)==len(infect))
             self.assertTrue(count_recover(pop)==len(recover))
